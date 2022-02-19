744bce5b208de9867fabbbf577e9530126ac04f3
kseniyaaltuhova <bogomolowakseniya@yandex.ru>
2022-02-19 18:03:25 +0300

    fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,12 @@ public class Main {
 
     long amount = 1000_60;
     boolean registered = true;
+    long expected = 30;
 
-    long result = service.calculate(amount, registered);
-    System.out.println(result);
+    long actual = service.calculate(amount, registered);
+
+    boolean passed = expected == actual;
+
+    System.out.println(passed);
   }
 }
